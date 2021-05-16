f6eee96d5d0f29eaf008b96d67bf60beb835e8e4
Nanda Kishor M Pai <nandakishormpai@gmail.com>
2021-05-17 01:31:50 +0530

    demo model ready

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,9 @@
 import requests
-import webbrowser
 import bs4
 import csv
-import re
 import os
 
 def build_csv():
-    quest_completed = []
-    quest_count = 0
     link = "https://www.malayalachalachithram.com/listsongs.php?tot=147&g=1414&p="
     with open("data/dataset.csv", "w") as ifile:
         writer = csv.writer(ifile)
@@ -16,48 +12,29 @@ def build_csv():
             res = requests.get(link + str(i))
             noStarchSoup = bs4.BeautifulSoup(res.text, "html.parser")
             table = noStarchSoup.findAll("table", {"class": "mdetails"})
-            for rows in table:
-                for row in rows:
-                    row_arr = []
-                    cells = row.findAll('td')
-                    if(not cells):
-                        continue
-                    row_arr.append(cells[2].getText())
-                    for a in cells[1].find_all('a', href=True):
-                        row_arr.append(a.getText()[:-4])
-                        row_arr.append(
-                            "https://www.malayalachalachithram.com/" + a['href'])
-                    csv_arr.append(row_arr)
+            rows = next(iter(table))
+            for row in rows:
+                row_arr = []
+                cells = row.findAll('td')
+                if(not cells):
+                    continue
+                row_arr.append(cells[2].getText())
+                for a in cells[1].find_all('a', href=True):
+                    row_arr.append(a.getText()[:-4])
+                    row_arr.append(
+                        "https://www.malayalachalachithram.com/" + a['href'])
+                csv_arr.append(row_arr)
         writer.writerows(csv_arr)
         ifile.close()
-
-def isManglish(str):
- 
-    # Regex to check string is
-    # alphanumeric or not.
-    regex = "^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$"
- 
-    # Compile the ReGex
-    p = re.compile(regex)
- 
-    # If the string is empty
-    # return false
-    if(str == None):
-        return False
- 
-    # Return if the string
-    # matched the ReGex
-    if(re.search(p, str)):
-        return True
-    else:
-        return False
-
 
 def lyrics_scrap():
     with open("data/dataset.csv", "r") as ifile:
         reader = csv.reader(ifile)
         song_num=0
+        links =[]
         for i,csv_row in enumerate(reader):
+            if(csv_row[2] in links): continue
+            else: links.append(csv_row[2])
             res = requests.get(csv_row[2])
             noStarchSoup = bs4.BeautifulSoup(res.text, "html.parser")
             table = noStarchSoup.findAll("table", {"id": "tbllyrics"})
@@ -71,7 +48,7 @@ def lyrics_scrap():
             lyrics = (cells[0].getText())
             writer = (cells[0]).findAll('em')
             if writer : lyrics = lyrics.replace(next(iter(writer)).getText(),"")
-            text_file = open("data/lyrics/"+str(song_num)+".txt", "w")
+            text_file = open("data/try/"+str(song_num)+".txt", "w")
             song_num+=1
             n = text_file.write(lyrics)
             text_file.close()
