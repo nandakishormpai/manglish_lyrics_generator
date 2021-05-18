891c7d4ebbf9dc74a490ea58dee22f3881bbcd72
Nanda Kishor M Pai <nandakishormpai@gmail.com>
2021-05-18 23:17:32 +0530

    web seems good

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+function visible(){
+    var x = document.getElementById("scroller");
+    x.style.display = "block";
+}
+
+
 function myFunction() {
     axios.get("https://manglish-lyrics-generator.herokuapp.com/").then(response => {
         console.log(response);
+        visible();
         document.getElementById("demo").innerHTML = response.data.lyrics;
     })
+    
 }
+
