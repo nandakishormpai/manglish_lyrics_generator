f927633253ad4ffec15f202206ad1b5f414b4251
Nanda Kishor M Pai <nandakishormpai@gmail.com>
2021-05-23 21:20:36 +0530

    trying to fix the api issue

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var x = "Generating...";
+
+// Globally calling so that We get a Lyrics ready for the user
 axios.get("https://manglish-lyrics-generator.herokuapp.com/").then(response => {
-        console.log(response);
+        console.log("first");
         x = response.data.lyrics;
 })
 
@@ -9,21 +11,22 @@ function visible(){
     y.style.display = "block";
 }
 
-
 function clearFunction(){
     var y = document.getElementById("scroller");
     y.style.display = "none";
 }
 
-
-
-
 function myFunction() {
+    var prev_lyrics;
     document.getElementById("demo").innerHTML = x;
     visible();
     axios.get("https://manglish-lyrics-generator.herokuapp.com/").then(response => {
-        console.log(response);
         document.getElementById("demo").innerHTML = x;
+        prev_lyrics = x;
+        console.log("second");
         x = response.data.lyrics;
+        if(prev_lyrics.localeCompare("Generating...")==0){
+            document.getElementById("demo").innerHTML = x;
+        }
     })    
 }
