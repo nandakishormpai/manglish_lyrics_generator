442b1b73acadf2568b190b1220e04f05e27ffd8a
Aswin Jayaji <56126732+aswinjayaji@users.noreply.github.com>
2021-05-25 15:47:35 +0530

    clean dir

---
+++ frontend/images/script.js
@@ -0,0 +1,29 @@
+var x = "Generating...";
+axios.get("https://manglish-lyrics-generator.herokuapp.com/").then(response => {
+        console.log(response);
+        x = response.data.lyrics;
+})
+
+function visible(){
+    var y = document.getElementById("scroller");
+    y.style.display = "block";
+}
+
+
+function clearFunction(){
+    var y = document.getElementById("scroller");
+    y.style.display = "none";
+}
+
+
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = x;
+    visible();
+    axios.get("https://manglish-lyrics-generator.herokuapp.com/").then(response => {
+        console.log(response);
+        document.getElementById("demo").innerHTML = x;
+        x = response.data.lyrics;
+    })    
+}
